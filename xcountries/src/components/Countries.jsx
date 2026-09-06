886f6b8b777b9dc96028b1df0ab058be6003ae75
Ashish-kumar2020 <ashishsinghk2020@gmail.com>
2026-09-06 23:00:37 +0530

    added error handling

--- xcountries/src/components/Countries.jsx
+++ xcountries/src/components/Countries.jsx
@@ -3,27 +3,42 @@ import CountryCard from "./CountryCard";
 
 const Countries = () => {
   const [countries, setCountries] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState("");
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         const response = await fetch(
-          "https://xcountries-backend.labs.crio.do/all",
+          "https://xcountries-backend.labs.crio.do/all"
         );
+
         if (!response.ok) {
-          throw new Error(`HTTP error : Status : ${response.status}`);
+          throw new Error(`HTTP Error: ${response.status}`);
         }
+
         const result = await response.json();
-        console.log(result);
         setCountries(result);
       } catch (error) {
-        console.log("Error while fetching countries : ", error);
+        setError(error.message);
+      } finally {
+        setLoading(false);
       }
     };
+
     fetchData();
   }, []);
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
+  if (error) {
+    return <p>{error}</p>;
+  }
+
   return (
-    <div className="grid grid-cols-6 gap-8  mt-20">
+    <div className="grid grid-cols-6 gap-8 mt-20">
       {countries.map((country,index) => (
         <CountryCard country={country} key={index} />
       ))}
